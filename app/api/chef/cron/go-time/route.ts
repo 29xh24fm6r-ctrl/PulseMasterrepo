2054b2a1fc37a128963d64179bb3b381089e532f
Matt <mpaller@yahoo.com>
2026-01-14 19:55:41 -0500

    chore: cleanup deleted routes

--- app/api/chef/cron/go-time/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { runChefGoTimeScan } from "@/lib/chef/notifications/goTimeScan";
-
-export const runtime = "nodejs";
-
-export async function POST() {
-    const res = await runChefGoTimeScan({});
-    return NextResponse.json({ ok: true, ...res });
-}
